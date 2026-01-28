9ccf79bbc8fd5d238c00bd9d874ea9b9d5bbf56a
Freetime Maker <jamieachatz@gmail.com>
2026-01-28 13:04:34 +0100

    Added and Changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    kotlin("kapt")
+    id("com.android.application")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp")
 }
 
 android {
-    namespace = "com.freetime.ssmp"
+    namespace = "com.freetime.ssmpc"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.freetime.ssmp"
+        applicationId = "com.freetime.ssmpc"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
@@ -43,8 +44,8 @@ android {
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:2.59")
+    kapt("com.google.dagger:hilt-compiler:2.59")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
