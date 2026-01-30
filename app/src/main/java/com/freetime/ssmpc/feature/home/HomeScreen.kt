d7a7dc89ecad0563edee1cf58a1ff85e54b16d59
Freetime Maker <jamieachatz@gmail.com>
2026-01-30 16:16:36 +0100

    Changed

--- app/src/main/java/com/freetime/ssmpc/feature/home/HomeScreen.kt
+++ app/src/main/java/com/freetime/ssmpc/feature/home/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -87,4 +88,10 @@ fun HomeScreen() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
